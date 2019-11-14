0868b1675d2399a0e4e450ea5dbdbc8d39861f27
Emily <bruner.emilym@gmail.com>
2019-11-14 17:10:53 -0500

    finished

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormikLogin from './components/MyForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikLogin />
+    </div>
+  );
+}
+
+export default App;
